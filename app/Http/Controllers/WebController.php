115b16451beaecf033dcb0536781376667800757
Gustavo Barragan <dev@droni.co>
2021-08-16 18:44:50 -0500

    fron angular first pub

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -11,12 +11,6 @@ use App\Models\OrderDetail;
 class WebController extends Controller
 {
   public function index() {
-    $order = Order::with('customer', 'order_detail')->where('customer_id', '>', 2)->limit(50)->get();
-    return response()->json($order);
-    $product = Product::first();    
-    $products = Product::limit(10)->get();    
-    $customers = Customer::limit(10)->get();
-    $orders = Order::limit(10)->get();
-    $ordersDetail = OrderDetail::limit(10)->get();
+    return view('web.index');
   }
 }
